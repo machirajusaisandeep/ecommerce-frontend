0e77f3beabac4dba94f1cdf53c226dadd7ae4934
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2024-04-19 15:40:32 +0530

    fix: added cart

---
+++ src/types/Cart.ts
@@ -0,0 +1,15 @@
+import { Shoe } from "./Shoe";
+
+interface CartProduct extends Shoe {
+  noOfItems: number;
+  totalItemPrice: number;
+  isCouponApplied: boolean;
+}
+
+interface Cart {
+  productList: Array<CartProduct>;
+  cartTotal: number;
+  couponsApplicable: Array<string>;
+  appliedCoupon: string;
+}
+export type { Cart };
